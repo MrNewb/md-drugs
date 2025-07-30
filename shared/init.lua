984a38108335ba4019bc8539e3d1b05dcd7744dc
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-07-30 10:55:20 -0400

    Remove Lua lang table and add SQL table builder to init
    - Removed the legacy Lua-based lang table as part of the migration to Community Bridge.
    - Integrated SQL table builder into the initialization process for automated table creation.

--- shared/init.lua
+++ shared/init.lua
@@ -3,3 +3,30 @@ Bridge = exports.community_bridge:Bridge()
 function locale(message, ...)
     return Bridge.Language.Locale(message, ...)
 end
+
+
+if IsDuplicityVersion() then
+
+
+    function InitializeSQL()
+        local result = MySQL.query.await('SHOW TABLES LIKE "deliveriesdealer"')
+        if #result == 0 then
+            MySQL.query.await('CREATE TABLE deliveriesdealer (id INT AUTO_INCREMENT PRIMARY KEY, cid VARCHAR(50), itemdata VARCHAR(50), timestart VARCHAR(50), timestop VARCHAR(50), maxtime VARCHAR(50), location VARCHAR(255))')
+            print(tostring(GetCurrentResourceName()).." Setting Up The  deliveriesdealer SQL Tables For You. This only runs once and is here to make it easy for you.")
+            Wait(1000)
+        end
+        local result2 = MySQL.query.await('SHOW TABLES LIKE "drugrep"')
+        if #result2 == 0 then
+            MySQL.query.await('CREATE TABLE drugrep (id INT AUTO_INCREMENT PRIMARY KEY, cid VARCHAR(50), drugrep VARCHAR(255), name VARCHAR(50))')
+            print(tostring(GetCurrentResourceName()).." Setting Up The  drugrep SQL Tables For You. This only runs once and is here to make it easy for you.")
+            Wait(1000)
+        end
+    end
+
+    AddEventHandler('onResourceStart', function(resourceName)
+        if resourceName ~= GetCurrentResourceName() then return end
+        InitializeSQL()
+    end)
+else
+
+end
